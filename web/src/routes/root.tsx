055788a0792b984dd6d9fe8cc218e64c790e680c
Alexander Greuter <greuteralexander@gmail.com>
2023-07-17 23:17:12 +0200

    Fix scrollbars always shown

--- web/src/routes/root.tsx
+++ web/src/routes/root.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router";
 export default function Root() {
   return (
     <Flex flexDirection="column" minH="100%">
-      <Box flexGrow="1" overflow="scroll">
+      <Box flexGrow="1" overflow="auto">
         <Outlet />
       </Box>
       <Box backgroundColor="gray.100">
