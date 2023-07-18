8999b591522da10cba31b9961c1bdeb8a1132575
Alexander Greuter <greuteralexander@gmail.com>
2023-07-18 17:01:19 +0200

    Fix tls

--- service/src/main.rs
+++ service/src/main.rs
@@ -6,13 +6,14 @@ use anyhow::Context;
 use deadpool::managed::{Object, Pool};
 use diesel::{
     Connection, ExpressionMethods, Insertable, PgConnection, QueryDsl, Queryable, Selectable,
-    SelectableHelper,
+    SelectableHelper, ConnectionResult, ConnectionError,
 };
 use diesel_async::{
     pooled_connection::AsyncDieselConnectionManager, AsyncPgConnection, RunQueryDsl,
 };
 use diesel_migrations::{embed_migrations, EmbeddedMigrations, MigrationHarness};
 use dotenvy::dotenv;
+use futures_util::{future::BoxFuture, FutureExt};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use warp::{
@@ -123,9 +124,43 @@ fn setup_db(database_url: &str) -> anyhow::Result<DBPool> {
         Err(err) => return Err(anyhow::anyhow!("Failed to run migrations: {}", err)),
     }
 
-    Pool::builder(AsyncDieselConnectionManager::new(database_url))
+    Pool::builder(
+        AsyncDieselConnectionManager::<AsyncPgConnection>::new_with_setup(
+            database_url,
+            establish_connection,
+        )
+    )
         .build()
         .context("Failed to init DB connection pool")
+}
+
+fn establish_connection(config: &str) -> BoxFuture<ConnectionResult<AsyncPgConnection>> {
+    let fut = async {
+        // We first set up the way we want rustls to work.
+        let rustls_config = rustls::ClientConfig::builder()
+            .with_safe_defaults()
+            .with_root_certificates(root_certs())
+            .with_no_client_auth();
+        let tls = tokio_postgres_rustls::MakeRustlsConnect::new(rustls_config);
+        let (client, conn) = tokio_postgres::connect(config, tls)
+            .await
+            .map_err(|e| ConnectionError::BadConnection(e.to_string()))?;
+        tokio::spawn(async move {
+            if let Err(e) = conn.await {
+                eprintln!("Database connection: {e}");
+            }
+        });
+        AsyncPgConnection::try_from(client).await
+    };
+    fut.boxed()
+}
+
+fn root_certs() -> rustls::RootCertStore {
+    let mut roots = rustls::RootCertStore::empty();
+    let certs = rustls_native_certs::load_native_certs().expect("Certs not loadable!");
+    let certs: Vec<_> = certs.into_iter().map(|cert| cert.0).collect();
+    roots.add_parsable_certificates(&certs);
+    roots
 }
 
 fn with_db(
